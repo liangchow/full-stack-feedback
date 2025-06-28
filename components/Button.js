3d7818990c7d2dcb1652e524c49aa506b3f2d248
liangchow <liangchern@gmail.com>
2025-06-27 23:49:55 -0700

    button

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,22 @@
-import { Open_Sans } from "next/font/google";
+import { Fugaz_One, Open_Sans } from "next/font/google";
 import React from 'react'
 
 const opensans = Open_Sans({subsets: ["latin"]})
+const fugaz = Fugaz_One({subsets: ["latin"], weight: ["400"]})
 
 export default function Button(props) {
 
     const {text, dark} = props
 
     return (
-        <button className={"rounded-full overflow-hidden duration-200 border-2 border-solid border-indigo-600 hover:opacity-60 cursor-pointer " + 
-        (dark ? "text-white bg-indigo-600 " : "text-indigo-600")}>
-            <p className={"px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 cursor-pointer "+opensans.className}>
-            {text}
-            </p>
+        // <button className={"rounded-full overflow-hidden duration-200 border-2 border-solid border-indigo-600 hover:opacity-60 cursor-pointer " + 
+        // (dark ? "text-white bg-indigo-600 " : "text-indigo-600")}>
+        //     <p className={"px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 cursor-pointer "+opensans.className}>
+        //     {text}
+        //     </p>
+        // </button>
+        <button className="block animate-background rounded-full bg-conic from-green-300 via-blue-600 to-red-300 bg-[length:_400%_400%] p-1 [animation-duration:_6s]">
+            <p className={"border px-6 sm:px-10 py-2 sm:py-3 " + fugaz.className}>{text}</p>
         </button>
     )
 }
