0c1182964e32df17188fbbb56d8535d927a67d41
liangchow <liangchern@gmail.com>
2025-07-01 16:33:39 -0700

    Update Panel.js
    Added image and h2

--- components/Panel.js
+++ components/Panel.js
@@ -1,7 +1,15 @@
+// import Image from 'next/image'
+import { Fugaz_One} from "next/font/google";
 import React from 'react'
+
+const fugaz = Fugaz_One({subsets: ["latin"], weight: ["400"]})
 
 export default function Panel() {
   return (
-    <div>Panel</div>
+    <div className="flex flex-col flex-1 items-center gap-4 p-4">
+        {/* <Image src={"lchow.jpg"} width={100} height={100} alt="dp" /> */}
+        <img className="max-w-[100px] w-full mx-auto rounded-full shadow-lg shadow-indigo-500/50" src="https://liangchow.github.io/assets/img/profile/lchow.jpg" alt="dp" />
+        <h2 className={"text-shadow-lg/10 text-indigo-500 items-center uppercase " + fugaz.className}>Liang Chow</h2>
+    </div>
   )
 }
