e52ace0ac975326dfd5f5ffa630b9a8fa9655354
liangchow <liangchern@gmail.com>
2025-07-04 16:18:38 -0700

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -9,10 +9,10 @@ export default function Card(props) {
         <li className='max-w-[800px] w-full mx-auto flex justify-between rounded-3xl p-3 border-2 border-solid border-indigo-300 '>
             {children}
             <div className={'flex p-2 gap-2 ' + (demo ? ' invisible' : '')}>
+                {/* Hide button */}
+                <button onClick={() => handleDeleteTodo(index)}><i className="fa-solid fa-eye-slash  text-indigo-600 hover:text-indigo-400"></i></button>
                 {/* Edit button */}
                 <button onClick={() => handleEditTodo(index)}><i className="fa-solid fa-pen-to-square text-indigo-600 hover:text-indigo-400"></i></button>
-                {/* Delete button */}
-                <button onClick={() => handleDeleteTodo(index)}><i className="fa-solid fa-trash  text-indigo-600 hover:text-indigo-400"></i></button>
             </div>
         </li>
     )
