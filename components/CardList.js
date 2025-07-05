b87faf5effa8a4bd6d8ce604de1de390e8bd9e59
liangchow <liangchern@gmail.com>
2025-07-05 12:43:54 -0700

    Update CardList.js

--- components/CardList.js
+++ components/CardList.js
@@ -10,10 +10,10 @@ export default function CardList(props) {
         {todos.map((todo, todoIndex) => {
             return(
                 <Card {...props} key={todoIndex} index={todoIndex} >
-                  <div className='grid grid-cols-3 justify-items-start gap-0.5 '>
-                    <div className='col-span-1'>{todo.peer}:</div>
-                    <div className='...'>{todo.comment}</div>
-                    <div className='col-span-1'>{todo.rating}</div>
+                  <div className='flex w-full justify-between gap-0.5 '>
+                    <div className='flex'>{todo.peer}:</div>
+                    <div className='flex text-left'>{todo.comment}</div>
+                    <div className='flex'>{todo.rating}</div>
                   </div>
                 </Card>
             )
