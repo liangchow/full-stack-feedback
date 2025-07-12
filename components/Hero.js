408d997390dde7f1231664de47dbf7b021d2b2b0
liangchow <liangchern@gmail.com>
2025-07-12 12:47:07 -0700

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -14,8 +14,8 @@ const demoUser = {firstName: "Liang", lastName: "Chow", src: "https://liangchow.
 export default function Hero() {
   return (
     <div className="flex flex-col py-4 md:py-8 gap-8 sm:gap-10">
-      <h1 className={"text-5xl sm:text-6xl md:text-7xl text-center " + fugaz.className}><span className="textGradient">Feedback</span> helps you track your 
-        <span className="textGradient"> daily</span> mood!</h1>
+      <h1 className={"text-5xl sm:text-6xl md:text-7xl text-center " + fugaz.className}><span className="textGradient">EndorseMe</span> helps you track your 
+        <span className="textGradient"> feedback</span> !</h1>
       <p className="max-w-[500px] w-full mx-auto text-lg sm:text-xl md:text-2xl text-center">Create your mood record and see how you feel <span className="font-semibold">every day.</span></p>
       <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
         <Button glow text="Sign Up" />
