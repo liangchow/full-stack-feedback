d900c662410e2f7daa45f1e1e323f05c811d1db4
liangchow <liangchern@gmail.com>
2025-07-12 21:56:12 -0700

    Card styling

--- components/Card.js
+++ components/Card.js
@@ -15,11 +15,8 @@ export default function Card(props) {
     }
 
         return (
-            <li className="max-w-[1000px] w-full mx-auto flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded-3xl border-2 border-solid border-indigo-300">
-                <div className="flex ml-2"> 
-                    {/* {headshot(index)} */}
-                    <div className="flex flex-col ml-2"> <span className="font-medium text-black">Tamaara Suiale</span> <span className="text-sm text-gray-400 truncate w-32">Are you going to business trip next week</span> </div>
-                </div>
+            <li className="max-w-[1000px] w-full mx-auto flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded-3xl border-2 border-solid border-indigo-300"> 
+                {children}
                 <div className='flex flex-col items-center p-2 gap-2 '>
                      {/* Show and NoShow button */}
                     <button onClick={() => handleToggleShow(index)}><i className={"text-indigo-600 hover:text-indigo-400 cursor-pointer transition " + (" fa-solid fa-eye-slash")}></i></button>
@@ -27,14 +24,4 @@ export default function Card(props) {
             </li>
         )
 
-        // https://bbbootstrap.com/snippets/tailwind-css-chat-list-template-search-75579288
-        // return (
-        //     <li className='max-w-[1000px] w-full mx-auto flex justify-between items-center rounded-3xl mt-2 p-2 border-2 border-solid border-indigo-300 '>
-        //         {children}
-        //         <div className='flex p-2 gap-2 '>
-        //             {/* Show and NoShow button */}
-        //             <button onClick={() => handleToggleShow(index)}><i className={"text-indigo-600 hover:text-indigo-400 " + (" fa-solid fa-eye-slash")}></i></button>
-        //         </div>
-        //     </li>
-        // )
 }
