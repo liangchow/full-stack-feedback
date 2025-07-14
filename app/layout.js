3cfbefd187df0ab67b2619cb575c86482181bf23
liangchow <liangchern@gmail.com>
2025-07-13 21:48:01 -0700

    AuthContext
    Initialize firebase operations

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Fugaz_One, Open_Sans } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import Logout from "@/components/Logout";
+import { AuthProvider } from "@/context/AuthContext";
 
 const opensans = Open_Sans({subsets: ["latin"]})
 const fugaz = Fugaz_One({subsets: ["latin"], weight:["400"]})
@@ -31,14 +32,16 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-      </head>
-      <body className={"max-w-[1000px] w-full mx-auto min-h-screen flex flex-col text-slate-800 text-sm sm:text-base md:text-lg " + opensans.className}>
-        {header}
-        {children}
-        {footer}
-      </body>
+      <AuthProvider>
+        <head>
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+        </head>
+        <body className={"max-w-[1000px] w-full mx-auto min-h-screen flex flex-col text-slate-800 text-sm sm:text-base md:text-lg " + opensans.className}>
+          {header}
+          {children}
+          {footer}
+        </body>
+      </AuthProvider>
     </html>
   );
 }
