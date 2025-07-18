79bc83c3397e20fd1cea6d637d80e0a6dcf13689
liangchow <liangchern@gmail.com>
2025-07-18 16:22:35 -0700

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -46,7 +46,7 @@ export default function Login() {
 
   return (
     <div className='flex flex-col flex-1 justify-center items-center gap-4'>
-      <h3 className={'text-4xl sm:text-5xl md:text-6xl '+fugaz.className}>{isRegister ? 'Register' : 'Log In'}</h3>
+      <h3 className={'text-4xl sm:text-5xl md:text-6xl '+fugaz.className}>{isRegister ? 'Create An Account' : 'Log In'}</h3>
       <p>You&#39;re one step away!</p>
 
       <input className='max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none ' 
@@ -59,7 +59,7 @@ export default function Login() {
       </div>
       <p className='text-center'>
         {isRegister ? 'Already have an account? ' : 'Don\'t have an account? '} 
-        <button onClick={() => setIsRegister(!isRegister)} className='text-indigo-600 cursor-pointer '>{isRegister ? 'Sign In' : 'Sign Up'}</button>
+        <button onClick={() => setIsRegister(!isRegister)} className='text-indigo-600 cursor-pointer '>{isRegister ? 'Log In' : 'Sign Up'}</button>
       </p>
     </div>
   )
