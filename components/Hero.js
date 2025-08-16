7091e3b61e416d287a3aec3ac949b410523a03a2
liangchow <liangchern@gmail.com>
2025-08-15 21:16:56 -0700

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -17,7 +17,7 @@ export default function Hero() {
     <div className="flex flex-col py-4 md:py-8 gap-8 sm:gap-10">
       <h1 className={"text-5xl sm:text-6xl md:text-7xl text-center " + fugaz.className}><span className="textGradient">Feedback</span> tracks your 
         <span className="textGradient"> feedback</span> from others!</h1>
-      <p className="max-w-[500px] w-full mx-auto text-lg sm:text-xl md:text-2xl text-center">Create your record and show how good it is <span className="font-semibold">to work with you.</span></p>
+      <p className="max-w-[500px] w-full mx-auto text-lg sm:text-xl md:text-2xl text-center">Your feedback, your <span className="font-semibold">superpowers!</span></p>
       <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
         <Link href={'/dashboard'}>
           <Button glow text="Sign Up" />
