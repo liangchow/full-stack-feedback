11817c03faa26199ae4bcc3f0f5f61368c453245
liangchow <liangchern@gmail.com>
2025-08-19 22:52:24 -0700

    Fix handleToggleStatus

--- components/CardList.js
+++ components/CardList.js
@@ -19,7 +19,7 @@ export default function CardList(props) {
     <ul className='flex flex-col flex-1 gap-1 p-4'>
         {todos.length == 0 ? <p className='text-indigo-600 text-center'>You have 0 feedback. Send request to your peer!</p> : todos.map((todo, todoIndex) => {
             return(
-                <Card {...props} key={todoIndex} index={todoIndex} >
+                <Card key={todoIndex} index={todoIndex} todo={todo} {...props} >
                   <div className={'flex w-full justify-between items-center gap-1 text-indigo-600 ' + (todo.status == true ? '' : 'opacity-50')}>
                     <div className='flex ml-1 w-15'>
                       {headshot(todo)}
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -7,7 +7,7 @@ import Login from './Login'
 import Loading from './Loading'
 import { useAuth } from '@/context/AuthContext'
 import {db} from '@/firebase'
-import { updateDoc } from 'firebase/firestore'
+import { updateDoc, doc } from 'firebase/firestore'
 
 
 const fugaz = Fugaz_One({subsets: ["latin"], weight: ["400"]})
