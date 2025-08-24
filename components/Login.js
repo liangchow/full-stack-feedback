75a62ec9f36e59eecc67343e8ef1b389151c7dea
liangchow <liangchern@gmail.com>
2025-08-23 22:53:31 -0700

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -9,6 +9,7 @@ const fugaz = Fugaz_One({subsets: ["latin"], weight: ["400"]})
 export default function Login() {
 
   const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [isRegister, setIsRegister] = useState(false)
@@ -49,6 +50,14 @@ export default function Login() {
     <div className='flex flex-col flex-1 justify-center items-center gap-4'>
       <h3 className={'text-4xl sm:text-5xl md:text-6xl '+fugaz.className}>{isRegister ? 'Create An Account' : 'Log In'}</h3>
       <p>You&#39;re one step away!</p>
+
+      <div className={isRegister ? ' ' : " visibility: hidden"}>
+        <input className='max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none ' 
+          value={firstName} onChange={(e) => {setFirstName(e.target.value)}} placeholder="First Name" type="text" />
+        <input className='max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none ' 
+          value={lastName} onChange={(e) => {setLastName(e.target.value)}} placeholder="Last Name" type="text" />
+      </div>
+      
 
       <input className='max-w-[400px] w-full mx-auto px-3 duration-200 hover:border-indigo-600 focus:border-indigo-600 py-2 sm:py-3 border border-solid border-indigo-400 rounded-full outline-none ' 
         value={email} onChange={(e) => {setEmail(e.target.value)}} placeholder="Email" type="text" />
