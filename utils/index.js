fd68f0e76ab4febccfdfed6a939dc63feaa7f281
liangchow <liangchern@gmail.com>
2025-08-26 21:31:00 -0700

    Update Card

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,5 @@
 export const demoData = [
-    {firstName:"Amina", lastName: "Patel", comment: "You consistently demonstrate attention to detail in your work. Try to speak up more in group discussions to share your valuable insights.", rating: 5, status: true, userID: "demoUser"},
+    {firstName:"Amina", lastName: "Patel", comment: "you consistently demonstrate attention to detail in your work. Try to speak up more in group discussions to share your valuable insights.", rating: 5, status: true, userID: "demoUser"},
     {firstName:"Luka", lastName: "Bianchi", comment: "Great initiative on the last project—you took ownership and delivered results. Continue to work on time management during tight deadlines.", rating: 3, status: true, userID: "demoUser"},
     {firstName:"Mei", lastName: "Tanaka", comment: "You show strong analytical thinking and problem-solving skills. Challenge yourself to take on more leadership roles.", rating: 4, status: true, userID: "demoUser"},
     {firstName:"Alex", lastName: "Tututututututu", comment: "This is a an example of card rendering. It must be loooonnnnnnngggggggg for it to work.", rating: 2, status: true, userID: "demoUser"},
