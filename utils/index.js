019a105f5262a154d13bc3af39ee1fab7c8f17b5
liangchow <liangchern@gmail.com>
2025-09-02 23:22:44 -0700

    Update index.js

--- utils/index.js
+++ utils/index.js
@@ -1,10 +1,10 @@
 export const demoData = [
-    {firstName:"Amina", lastName: "Patel", comment: "you consistently demonstrate attention to detail in your work. Try to speak up more in group discussions to share your valuable insights.", rating: 5, status: true, userID: "demoUser"},
-    {firstName:"Luka", lastName: "Bianchi", comment: "Great initiative on the last project—you took ownership and delivered results. Continue to work on time management during tight deadlines.", rating: 3, status: true, userID: "demoUser"},
-    {firstName:"Mei", lastName: "Tanaka", comment: "You show strong analytical thinking and problem-solving skills. Challenge yourself to take on more leadership roles.", rating: 4, status: true, userID: "demoUser"},
-    {firstName:"Alex", lastName: "Tututututututu", comment: "This is a an example of card rendering. It must be loooonnnnnnngggggggg for it to work.", rating: 2, status: true, userID: "demoUser"},
-    {firstName:"Jane", lastName: "Woo", comment: "Hi there!", rating: 1, status: true, userID: "demoUser"},
-    {firstName:"John", lastName: "Doe", comment: "Hello! This is a demo data", rating: 5, status: true, userID: "demoUser"},
-    {firstName:"Allie", lastName: "Javo", comment: "Welcome abroad.", rating: 3, status: true, userID: "demoUser"},
-    {firstName:"Mia", lastName: "Dell", comment: "Demo data", rating: 4, status: true, userID: "demoUser"}
+    {firstName:"Amina", lastName: "Patel", comment: "you consistently demonstrate attention to detail in your work. Try to speak up more in group discussions to share your valuable insights.", rating: 5, status: true},
+    {firstName:"Luka", lastName: "Bianchi", comment: "Great initiative on the last project—you took ownership and delivered results. Continue to work on time management during tight deadlines.", rating: 3, status: true},
+    {firstName:"Mei", lastName: "Tanaka", comment: "You show strong analytical thinking and problem-solving skills. Challenge yourself to take on more leadership roles.", rating: 4, status: true},
+    {firstName:"Alex", lastName: "Tututututututu", comment: "This is a an example of card rendering. It must be loooonnnnnnngggggggg for it to work.", rating: 2, status: true},
+    {firstName:"Jane", lastName: "Woo", comment: "Hi there!", rating: 1, status: true},
+    {firstName:"John", lastName: "Doe", comment: "Hello! This is a demo data", rating: 5, status: true},
+    {firstName:"Allie", lastName: "Javo", comment: "Welcome abroad. Good luck!", rating: 3, status: true},
+    {firstName:"Mia", lastName: "Dell", comment: "This is a demo data", rating: 4, status: true}
 ]
