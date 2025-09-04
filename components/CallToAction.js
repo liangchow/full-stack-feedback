1f9b696d45bbf1d690eed9cef5df8a505c7c5154
liangchow <liangchern@gmail.com>
2025-09-03 22:39:37 -0700

    Update CallToAction.js

--- components/CallToAction.js
+++ components/CallToAction.js
@@ -7,16 +7,18 @@ import { useAuth } from '@/context/AuthContext'
 export default function CallToAction() {
     const {currentUser} = useAuth()
 
+    // If user is logged in, display Return to Dashboard button.
     if (currentUser){
         return(
             <div className='mx-w-[600px] mx-auto w-full'>
                 <Link href={'/dashboard'}>
-            <Button full text='Go to Dashboard' />
+            <Button full text='Return to Dashboard' />
         </Link>
             </div>
         )
     }
 
+    // Otherwise, shows Sign Up and Login buttons.
   return (
     <div className='grid grid-cols-2 gap-4 p-2 w-fit mx-auto'>
         <Link href={'/dashboard'}>
