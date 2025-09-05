06bddc28b67d3ff61cbe3f7fa39ed144581ee1aa
liangchow <liangchern@gmail.com>
2025-09-05 14:57:36 -0700

    Update firebase.js
    Add getFunctions

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getFunctions } from "firebase/functions"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app)
 
 export const auth = getAuth(app)
 export const db = getFirestore(app)
+export {functions}
